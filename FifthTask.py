bef2388e398172de40e30cbfb0d177d891366695
yanasobolieva <soboleva.yana.sys@gmail.com>
2019-10-17 01:56:35 +0300

    Updated 5th task

--- FifthTask.py
+++ FifthTask.py
@@ -1,4 +1,10 @@
 digits_list = [10, -15, 3, 8, 0, 9, -6, 13, -1, 5]
+print(digits_list)
 
+for i in range(len(digits_list)):
+    if digits_list[i] < 0:
+        digits_list[i] = -1
+    elif digits_list[i] > 0:
+        digits_list[i] = 1
 
-print(digits_list[1]>0)
+print(digits_list)
